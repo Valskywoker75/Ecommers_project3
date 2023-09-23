2cee5fb98f8c95dbd7489567e2d7ff7179ce3834
Valskywoker75 <38358072+Valskywoker75@users.noreply.github.com>
2023-09-23 22:00:03 +0300

    Update urls.py
    23.09.2023

--- ecommerce_project3/urls.py
+++ ecommerce_project3/urls.py
@@ -9,10 +9,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('product/', include('products.urls')),
     path('category/<int:pk>/', product_views.category_detail, name='category_detail'),
+    path('cart/<int:pk>/', product_views.addcart, name='addcart'),
+    path('cart/', product_views.cart, name='cart'),
     path('create_game_user/', product_views.create_game_user, name='create_game_user'),
     path('login_page/', product_views.login_view, name='login_page'),
     path('success_auth/', product_views.login_view, name='success_auth'),
     path('logout/', product_views.logout, name='success_auth'),
+    path('cartclear/', product_views.cartclear, name='cartclear'),
 ]
 
 if settings.DEBUG:
