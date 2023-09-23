8bab68a9d0c48bf864d103cbe5f61cf6d4c3c492
Valskywoker75 <38358072+Valskywoker75@users.noreply.github.com>
2023-09-23 21:57:53 +0300

    Update views.py
    23.09.2023

--- products/views.py
+++ products/views.py
@@ -6,15 +6,44 @@ from .forms import UserForm
 
 def index(request):
     categories = Category.objects.all()
-    return render(request, 'products/home.html', {'categories': categories})
+    if 'cart' in request.session:
+        cart_count = len(request.session['cart'].split(','))
+    else:
+        cart_count = 0
+    return render(request, 'products/home.html', {'categories': categories, 'request':request, 'cart_count': cart_count})
 
 def category_detail(request, pk):
+    if 'cart' in request.session:
+        cart_count = len(request.session['cart'].split(','))
+    else:
+        cart_count = 0
     category = get_object_or_404(Category, pk=pk)
     products = Product.objects.filter(category=category)
-    return render(request, 'products/category_detail.html', {'category': category, 'products': products})
+    return render(request, 'products/category_detail.html', {'category': category, 'products': products, 'cart_count': cart_count})
+
+def addcart(request, pk):
+    if 'cart' in request.session:
+        request.session['cart'] = request.session['cart']+', '+str(pk)
+    else:
+        request.session['cart'] = str(pk)
+    return redirect('/product/product/'+str(pk))
+def cart(request):
+    if 'cart' in request.session:
+        cart_count = len(request.session['cart'].split(','))
+    else:
+        cart_count = 0
+    if 'cart' in request.session:
+        carts = request.session['cart'].split(',')
+        cart_list = Product.objects.filter(id__in=carts)
+        return render(request, 'products/list_cart.html',{'request': request, 'cart_list':cart_list, 'cart_count':cart_count})
+
 
 def product_detail(request, product_id):
-    product = get_object_or_404(Product, pk=product_id)
+    if 'cart' in request.session:
+        cart_count = len(request.session['cart'].split(','))
+    else:
+        cart_count = 0
+    product = Product.objects.filter(id=product_id).first()
     comments = Comment.objects.filter(product=product).order_by('-created_at')
 
     if request.method == 'POST':
@@ -27,7 +56,7 @@ def product_detail(request, product_id):
     else:
         form = CommentForm()
 
-    return render(request, 'products/product_detail.html', {'product': product, 'comments': comments, 'form': form})
+    return render(request, 'products/product_detail.html', {'request':request, 'product': product, 'comments': comments, 'form': form, 'cart_count': cart_count})
 
 
 def create_game_user(request):
@@ -39,42 +68,31 @@ def create_game_user(request):
     else:
         form = UserForm()
 
-    return render(request,'mygameapp/create_game_user.html',{'form': form},)
+    return render(request,'mygameapp/create_game_user.html',{'form': form})
 
 def success_auth(request):
-    print(1)
     if request.user.is_authenticated:
         return render(request, 'mygameapp/success_auth.html')
     else:
         return redirect('login_page')
 
-
-
 def login_view(request):
     if request.method == 'POST':
         #categories = Category.objects.all()
-        print(request.POST['nickname'])
         if User.objects.filter(nickname=request.POST['nickname']).exists():
-            print('login')
             request.session['login'] = request.POST['nickname']
             return redirect('/')
         else:
             return redirect('/login_page')
     else:
         return render(request, 'mygameapp/login_page.html')
+
 def logout(request):
     if 'login' in request.session:
         del request.session['login']
-    return redirect('/login_page')
+    return redirect('/')
 
-
-"""class CustomLoginView(LoginView):
-    template_name = 'mygameapp/login_page.html'
-    success_message = "Авторизация прошла успешно!"
-    success_url = '/success_auth/'
-    redirect_authenticated_user = False
-
-    def form_valid(self, form):
-        response = super().form_valid(form)
-        messages.success(self.request, self.success_message)
-        return response"""
+def cartclear(request):
+    if 'cart' in request.session:
+        del request.session['cart']
+    return redirect('/')
