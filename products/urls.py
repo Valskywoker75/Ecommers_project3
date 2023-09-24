0e375ecfe79c51c4807eef740b71dcf83613d089
Valskywoker75 <38358072+Valskywoker75@users.noreply.github.com>
2023-09-24 15:49:16 +0300

    Update urls.py

--- products/urls.py
+++ products/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('category/<int:pk>/', views.category_detail, name='category_detail'),
     path('product/<int:product_id>/', views.product_detail, name='product_detail'),
-
+    path('contacts/', views.contact, name='contact'),
 ]
 
 
